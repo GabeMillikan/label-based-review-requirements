e33e75e2bb0ecae8632b5ca8ddc6e3038427ff75
GabeMillikan <44247924+GabeMillikan@users.noreply.github.com>
2023-10-12 20:57:56 -0700

    and also rebuild

--- .github/actions/label-based-review-requirements/dist/index.js
+++ .github/actions/label-based-review-requirements/dist/index.js
@@ -30003,14 +30003,7 @@ const core = __nccwpck_require__(3370);
 const github = __nccwpck_require__(289);
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    console.log("HELLO WORLD!!");
 } catch (error) {
     core.setFailed(error.message);
 }
